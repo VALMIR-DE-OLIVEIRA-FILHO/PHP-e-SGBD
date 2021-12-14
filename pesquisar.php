7755185dbb33431b75927410a0092311544ab6ee
olivervalmir <79705680+Valmir-de-Oliveira-Filho@users.noreply.github.com>
2021-12-14 20:28:31 -0300

    modificações do PHP

--- pesquisar.php
+++ pesquisar.php
@@ -17,7 +17,7 @@
         $resultado_sql= "SELECT * FROM carrotb WHERE modelo LIKE '%$pesquisar%'  ";
     }
 
-    $result_cars = mysqli_query($conn, $resultado_sql);
+    $result_sql = mysqli_query($conn, $resultado_sql);
  
    
     
@@ -40,17 +40,17 @@
     <body>
 
         <?php
-while($rows_cars = mysqli_fetch_array($result_cars)){
+while($rows_sql = mysqli_fetch_array($result_sql)){
   
     echo'<div id="dois">';
     echo'<div class="alinhamento">';
-    echo '<strong>Modelo:</strong> <p class="cola">'.$rows_cars['modelo'].'</p><br>';
+    echo '<strong>Modelo:</strong> <p class="cola">'.$rows_sql['modelo'].'</p><br>';
     echo'</div> ';
     echo'<div class="alinhamento">';
-    echo '<strong id="strong-foto">Foto:</strong> <img class="img" src="./img/'.$rows_cars['foto'].'"/>';
+    echo '<strong id="strong-foto">Foto:</strong> <img class="img" src="./img/'.$rows_sql['foto'].'"/>';
     echo'</div> ';
     echo'<div class="alinhamento">';
-    echo ' <strong>Preço:</strong> <p class="cola">R$'.$rows_cars['preço'].'<br>';
+    echo ' <strong>Preço:</strong> <p class="cola">R$'.$rows_sql['preço'].'<br>';
     echo'</div> ';
     echo'<div class="alinhamento">';
     echo'<a id="botao" href="form.php">sair</a><br /><br />';
